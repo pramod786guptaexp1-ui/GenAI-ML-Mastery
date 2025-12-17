e9e32aba3d20a83ad634dfb4b58df2cd53633c43
pramod786guptaexp1-ui <pramod786gupta.exp1@gmail.com>
2025-12-17 23:41:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ with st.sidebar:
         menu_icon="cast", # optional
         default_index=0, # optional
         orientation="Horizontal",
+        styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "18px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
     )
 
 # --- HOME SECTION ---
